a4b2b13a596ff703f79f146e16e22d353a74de82
Bruno <brunoconcli@gmail.com>
2024-03-04 11:13:06 -0300

    Changed `App` file to v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AudioPlayer from './components/AudioPlayer';
+import A5 from './audio/piano-notes/A5.mp3'
+import G5 from './audio/piano-notes/Gb5.mp3'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AudioPlayer audio={A5}/>
+      <AudioPlayer audio={G5}/>
+
     </div>
   );
 }
